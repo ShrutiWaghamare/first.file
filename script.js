52e904e7e362e805cdb51f1eda230eebbac967bb
Shruti Waghamare <shrutiw796Qgmail.com>
2023-05-20 21:41:53 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+let menu = document.querySelector('#menu-btn');
+let navbar = document.querySelector('.header .navbar');
+
+menu.onclick = () => {
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+}
+
+window.onscroll = () => {
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+};
+
+var swiper = new Swiper(".home-slider", {
+    loop: true,
+
+    navigation: {
+        nextE1: ".swiper-button-next",
+        prevE1: ".swiper-button-prev",
+    },
+});
