6b659080247d6a2305637ff551d178b61d7b63bb
Shruti Waghamare <shrutiw796Qgmail.com>
2023-05-22 20:40:45 +0530

    first cummit

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,16 @@
+url="https://www.apple.com/in/shop/buy-iphone/iphone-14-pro"
+
+# page1=requests.get(url,headers=headers)
+# Soup1=BeautifulSoup(page1.content,"html.parser")
+# Soup2=BeautifulSoup(Soup1.prettify(),"html.parser") 
+
+# price1=Soup2.find(class_="nowrap").get_text()
+# title1=Soup2.find(class_="form-selector-title")
+# # title1=Soup2.find('div',attrs={"id":"20ab0580-f25e-11ed-8d70-9988fd9ce209_label","class":"form-selector-title"}).get_text()
+
+# price1=price1.strip()
+# title1=title1.strip()
+
+# print("Official Website :")
+# print(title1)
+# print("Price - ",price1)
